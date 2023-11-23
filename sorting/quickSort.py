f0adf1837dd1da6c52b9fd8f41330c186346e850
Pumudu Nadun Rajakaruna <e19306@eng.pdn.ac.lk>
2023-11-23 17:53:16 +0530

    add merge sort file

--- sorting/quickSort.py
+++ sorting/quickSort.py
@@ -1,13 +1,14 @@
-dataSet = [7, 6, 10, 5, 9, 2, 1, 15, 7]
-
-def quickSort(arr):
+# Quick sort : Iterative method
+def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     else:
         pivot = arr[0]
-        less = [x for x in arr[1:] if x <= pivot]
-        greater = [x for x in arr[1:] if x > pivot]
-        return quick_sort(less) + [pivot] + quick_sort(greater)
+        less = [], greater = []
+        for x in arr[1:]:
+            if x <= pivot:
+                less.append(x)
+            else:
+                greater.append(x)
 
-sorted_list = quickSort(dataSet)
-print(sorted_list)
+        return quick_sort(less) + [pivot] + quick_sort(greater)
