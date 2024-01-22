93e12412d9d00da5f81e55c27da370511d656d69
Pumudu Nadun Rajakaruna <e19306@eng.pdn.ac.lk>
2024-01-22 23:43:10 +0530

    create binary search tree

---
+++ data-structures/Tree/binary_search_tree.py
@@ -0,0 +1,62 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    def add_child(self, data):
+        if self.data == data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def search(self, data):
+        if self.data == data:
+            return True
+
+        if data < self.data:
+            if self.left:
+                return self.left.search(data)
+            else:
+                return False
+
+        if data > self.data:
+            if self.right:
+                return self.right.search(data)
+            else:
+                return False
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for number in elements:
+        root.add_child(number)
+
+    return root
+
+if __name__ == '__main__':
+    numbers = [17,23,44,51,22,11,8,99]
+    numbers_trees = build_tree(numbers)
+    print(numbers_trees.in_order_traversal())
+    print(numbers_trees.search(4))
