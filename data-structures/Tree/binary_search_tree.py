ca461fbe5d1348e7772418a6a7782a9a6d0967b4
Pumudu Nadun Rajakaruna <115540884+PumuduRajakaruna@users.noreply.github.com>
2024-01-24 19:07:51 +0530

    Update binary_search_tree.py

--- data-structures/Tree/binary_search_tree.py
+++ data-structures/Tree/binary_search_tree.py
@@ -3,6 +3,7 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
     def add_child(self, data):
         if self.data == data:
             return
@@ -47,6 +48,39 @@ class BinarySearchTreeNode:
                 return self.right.search(data)
             else:
                 return False
+
+    def search_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.search_max()
+
+    def search_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.search_min()
+
+    def delete(self, data):
+        if data < self.data:
+            if self.left:
+                self.left = self.left.delete(data)
+        elif data > self.data:
+            if self.right:
+                self.right = self.right.delete(data)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.left
+
+            min_val = self.right.search_min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+
+        return self
+
+
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
 
@@ -55,8 +89,15 @@ def build_tree(elements):
 
     return root
 
+
 if __name__ == '__main__':
-    numbers = [17,23,44,51,22,11,8,99]
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
     numbers_trees = build_tree(numbers)
     print(numbers_trees.in_order_traversal())
-    print(numbers_trees.search(4))
+    # print(numbers_trees.search(4))
+    # print("Maximum number: ", numbers_trees.search_max())
+    # print("Minimum number: ", numbers_trees.search_min())
+    numbers_trees.delete(20)
+    print(numbers_trees.in_order_traversal())
+    numbers_trees.delete(9)
+    print(numbers_trees.in_order_traversal())
